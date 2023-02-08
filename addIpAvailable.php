808460c55efa5856443c467d25ed8063d932cd0b
roulios44 <jules.croizier@gmail.com>
2023-02-08 02:09:04 +0100

    manualy add server => OK

---
+++ addIpAvailable.php
@@ -0,0 +1,19 @@
+<?php
+try{
+    include_once "header.php";
+    include_once "dbHandler.php";
+    $encoded = file_get_contents("php://input");
+    $decode = json_decode($encoded, true);
+    $serverIp = $decode["ip"];
+    $arrayData = array(
+        "serverIp"=>$serverIp,
+    );
+        $db = new dbHandler();
+        $db->insert($arrayData,"addressAvalaible");
+
+}catch(ERROR $e){
+    echo $e;
+}catch(Exception $e){
+    echo $e;
+}
+?>
